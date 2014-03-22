1f2273e92fe5c5f0ca03be3c7254c5f5d77a7840
Roman Koshlyak <dev.rkoshlyak@gmail.com>
2014-03-22 00:55:50 -0700

    Remove query arguments from facebook comment url. This fixes #3

--- src/main/dart/GromTable/web/facebookcommentsview.dart
+++ src/main/dart/GromTable/web/facebookcommentsview.dart
@@ -8,11 +8,21 @@ class FacebookCommentsView extends PolymerElement {
   }
   
   String getIframeSrc(String url) {
+    Uri documentUri = Uri.parse(url);
+    // Note: facebook ads queries for tracking purpouse.
+    // http://gromstol.org/?fb_action_ids=10200884957525918&fb_action_types=og.comments#document4dd3000400000000000000f6c3c01df2
+    Uri withoutQuery = new Uri(
+        // Note: we want same comments for http and https
+        scheme : 'http',
+        host : documentUri.host,
+        port : documentUri.port,
+        path : documentUri.path,
+        fragment : documentUri.fragment);
     Uri uri = new Uri.http(
       Host.apiDomain,
       '/api/facebook_comments',
       {
-        'url': url,
+        'url': withoutQuery.toString(),
       }
     );
     return uri.toString();
